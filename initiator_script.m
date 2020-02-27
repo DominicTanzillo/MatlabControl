14265fddae957520b2f247f936ae12b807a8aca3
DTanzillo <56934312+DTanzillo@users.noreply.github.com>
2020-02-27 15:32:29 -0600

    Initial commit

---
+++ initiator_script.m
@@ -0,0 +1,10 @@
+%% If Path has been specified
+% The following text should be sufficient if you have followed the README
+% file for this project.
+
+system('python2 ps3rover.py')
+
+%% Specify Path exactly
+% Uncomment the following line section and edit the path.
+
+% system('python2 C:\path\to\project\MatlabControl\ps3rover20.py')
